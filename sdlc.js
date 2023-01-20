6af6621faf4d3a04d0fc327d679b1738b41ecd30
Caleb Holdaway <cdholdaway@gmail.com>
2023-01-19 19:37:29 -0700

    Unit 1 Assessment

---
+++ sdlc.js
@@ -0,0 +1,25 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Planning
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const analysisOfRequirements = "This is the first and fundamental stage of SDLC. Business analysts gather requirements from their customers, target market, and industry experts to create a Business Specification (BS) document. Other organizations and teams may refer to this document as Customer Requirement Specification (CRS). The intent of this document is to outline current pain points that developers should strive to eliminate through the power of software. It can be a useful resource to help the team discover innovative methods to change and improve their products. "
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const design = "This stage focuses on designing the product. It involves product architects and developers who will ideate and present a design of the product. They may present more than one design approach, and these ideas are documented in a Design Document Specification (DDS)."
+const implementation = "Production commences and the product is built. The programming code is built per the DDS (Stage 3), so the product can be created with the utmost efficiency. Developers use various tools and programming languages to build the code. These are selected based on the demands of the software being developed."
+const testingAndIntegration = "The development team tests the software for errors and deficiencies. Does the software produce the right results? Does it fulfill the requirements and objectives initially outlined in the SDLC? These are examples of key questions that could be asked during the testing phase. Once the software has undergone testing and QA, it is delivered to the customer. This stage usually involves deployment engineers who make software available to customers. They may install the software at a company and/or help individual customers run the application on their computers."
+const maintain = "To help mitigate the amount of maintenance that needs to be done, teams may choose to first release the product to a smaller population of customers. This can offer insight into how the product is performing and the development teams can make any last adjustments prior to its final release."
