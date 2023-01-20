25835dad5d684777dca8c731f413264e8fccf136
Caleb Holdaway <cdholdaway@gmail.com>
2023-01-19 20:23:23 -0700

    Unit 1 Assessment

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+var gitDefinition = "An open source control system designed to handle code management."
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+var gitHubDefinition = "A website where users can upload their files to allow others to see what projects they've worked on."
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+var gitInitDefinition = "A command that creates a new Git repository, which can be used to convert existing projects to a repository."
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+var gitCloneDefinition = "A command that is used to grab an exisiting repository and make a clone/copy of that repository at a different location."
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+var gitStatusDefinition = "A command that displays the state of the directory and staging area, to show which changes have happened."
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,8 @@
 */
 
 //CODE HERE
-
+var gitAddDefinition = "A command that adds a change in the working directory of the staging area."
+var gitAddCode = "git add ."
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +65,8 @@
 */
 
 //CODE HERE
-
+var gitCommitDefinition = "A command that captures a snapshot of the project's current changes."
+var gitCommitCode = "git commit -m 'initial commit'"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +75,4 @@
 */
 
 //CODE HERE
+var gitPushDefinition = "A command used to upload a local repository to a remote repository."
